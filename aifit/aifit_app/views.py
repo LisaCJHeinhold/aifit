d53e5ff0082ee0bbb5efe8df3ff2352a15ee57a0
Elijah Elliott <elliott.t.elijah@gmail.com>
2024-02-15 20:15:47 -0700

    I imolement a class Firebase for cred and db is stored.

--- aifit/aifit_app/views.py
+++ aifit/aifit_app/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
-from fire.firebase import firebaseInit
-
+# from fire.firebase import firebaseInit, Firebase
+from fire.firebase import Firebase
 
 
 # Create your views here.
 def home(request):
-    firebaseInit()
+    # firebaseInit()
+    # Assuming the credentials file is located at 'path/to/your/credentials.json'
+    firebase_instance = Firebase()
+    print(firebase_instance)
+    
+     # Now you can use the firebase_instance to interact with Firebase services
+    data = firebase_instance.get_data(collection='your_collection', document='your_document')
+    print(data)
+    
     return render(request, 'aifit_app/index.html')
