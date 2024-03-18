9e6933a3a523646334a9997683edebc46ec90677
bostonwilliamss1 <bostonwilliamss1@gmail.com>
2024-03-18 09:50:25 -0600

    created functions folder

---
+++ aifit/aifit_app/static/functions/functions.py
@@ -0,0 +1,14 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+cred = credentials.Certificate("/Users/bostonwilliams/Desktop/GitHub/aifit/aifit/fire/aifit-42d60-4b74ea669715.json")
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+def get_goals(): 
+    docs = db.collection('goals').get()
+    for doc in docs:
+        goals_dictionary = doc.to_dict()
+        print(goals_dictionary['goal'])
