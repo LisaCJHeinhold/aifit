60d75f482fddedb55a1ef8e8316566142f760414
Alyssa Kucharyski <adkucharyski@gmail.com>
2024-04-03 20:39:28 -0600

    10k changes lol

---
+++ aifit/aifit_app/static/functions/functions.py
@@ -0,0 +1,91 @@
+# BOSTON IS WORKING WITH THIS
+from django.shortcuts import render
+from firebase_admin import credentials, firestore
+from datetime import datetime 
+
+db = firestore.client()
+
+def get_goals():
+    goals = []
+    docs = db.collection('goals').get()
+    for doc in docs:
+        goals_dictionary = doc.to_dict()
+        goals.append(goals_dictionary['goal'])
+    return goals
+
+def get_todays_date():
+    current_date = datetime.today()
+    day_of_week = current_date.strftime('%A')
+    return day_of_week
+
+def get_todays_workout():
+    docs = db.collection('user_workouts').get()
+    today = get_todays_date()
+
+    for doc in docs:
+        workout_data = doc.to_dict()
+        if workout_data['day_schedule'] == today:
+            return workout_data.get('type')
+    return "error"
+
+def get_time():
+    docs = db.collection('user_workouts').get()
+    today = get_todays_date()
+
+    for doc in docs:
+        workout_data = doc.to_dict()
+        if workout_data['day_schedule'] == today:
+            return workout_data['time']
+    return "error"
+
+def get_num_of_exercises():
+    docs = db.collection('user_workouts').get()
+    today = get_todays_date()
+
+    for doc in docs:
+        workout_data = doc.to_dict()
+        if workout_data['day_schedule'] == today:
+            return workout_data['number_exercises']
+    return "error"
+
+def get_workout_ideas():
+    docs = db.collection('user_workouts').get()
+    today = get_todays_date()
+
+    for doc in docs:
+        workout_data = doc.to_dict()
+        if workout_data['day_schedule'] == today:
+            return workout_data['workout_ideas']
+    return "error"
+
+def get_body_fat():
+    docs = db.collection('graph').get()
+
+    for doc in docs:
+        graph_data = doc.to_dict()
+        return graph_data['body_percentage_fat']
+    
+
+def get_muscle_mass():
+    docs = db.collection('graph').get()
+
+    for doc in docs:
+        graph_data = doc.to_dict()
+        return graph_data['muscle_mass_percentage']     
+
+def get_weight():
+    docs = db.collection('graph').get()
+
+    for doc in docs: 
+        graph_data = doc.to_dict()
+        return graph_data['weight']
+    
+def get_workout():
+    docs = db.collection('user_workouts').get()
+    today = get_todays_date()
+
+    for doc in docs:
+        workout_data = doc.to_dict()
+        if workout_data['day_schedule'] == today:
+            return workout_data['type']
+    return "error"
